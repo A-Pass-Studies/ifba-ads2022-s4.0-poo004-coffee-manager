448cd5e9b471ee671c980ec0b6a0a36efa62574d
Arthur Lucas Oliveira Santos <santosarthuroliver@gmail.com>
2025-02-27 10:19:33 -0300

    Controladores pedidos e sub tela

--- src/main/java/br/com/arthur/javafx/FXMLController.java
+++ src/main/java/br/com/arthur/javafx/FXMLController.java
@@ -6,11 +6,16 @@ Put header here
  */
 
 import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import java.io.IOException;
 
 public class FXMLController implements Initializable {
     
@@ -20,6 +25,26 @@ public class FXMLController implements Initializable {
     @FXML
     private void btnClickAction(ActionEvent event) {
         lblOut.setText("Hello World!");
+    }
+    
+    @FXML
+    private void handleAdicionarPedido(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/OrderManagerADD.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Adicionar Pedido");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();
+    }
+
+    @FXML
+    private void handleFinalizarPedido(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/OrderManagerClose.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Finalizar Pedido");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();
     }
     
     @Override
---
+++ src/main/java/br/com/arthur/javafx/OrderCloseController.java
@@ -0,0 +1,28 @@
+package br.com.arthur.javafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class OrderCloseController {
+
+    @FXML
+    private TextField numeroPedidoFinalizarField;
+
+    @FXML
+    private void finalizarPedido(ActionEvent event) {
+        String numeroPedido = numeroPedidoFinalizarField.getText();
+
+        // back end pra finalizar pedido
+
+        Stage stage = (Stage) numeroPedidoFinalizarField.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+        Stage stage = (Stage) numeroPedidoFinalizarField.getScene().getWindow();
+        stage.close();
+    }
+}
